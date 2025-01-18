45697c22d26d47f57dbe1bf2ba78a35cc5fa46a9
rvfx04 <rvfx04@gmail.com>
2025-01-18 13:16:35 -0500

    Update gantt2BD.py

--- gantt2BD.py
+++ gantt2BD.py
@@ -327,7 +327,7 @@ if st.button("Ejecutar Consulta"):
 
                 # Procesar los datos para el gráfico de Gantt
                 f_emision = pd.to_datetime(df['F_EMISION'].iloc[0])
-                dias = df['DIAS'].iloc[0]
+                #dias = df['DIAS'].iloc[0]
 
                 # Cálculo de las fechas de inicio y fin
                 
