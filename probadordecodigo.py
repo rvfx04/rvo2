16a63babd235217b67b17c7fd6690cb94e6d0220
rvfx04 <rvfx04@gmail.com>
2025-02-27 14:33:32 -0500

    Update probadordecodigo.py

--- probadordecodigo.py
+++ probadordecodigo.py
@@ -429,12 +429,12 @@ if st.button("Ejecutar Consulta"):
                                 diferencia_dias = (fecha_fin - fecha_actual).days
                                 
                                 # Evitar división por cero
-                                if (fecha_fin - fecha_inicio).days > 0:
+                                #if (fecha_fin - fecha_inicio).days > 0:
                                     porcentaje_avance = ((fecha_actual - fecha_inicio).days / (fecha_fin - fecha_inicio).days) * 100
                                     # Limitar el porcentaje entre 0 y 100
-                                    porcentaje_avance = max(0, min(100, porcentaje_avance))
-                                else:
-                                    porcentaje_avance = 100 if fecha_actual >= fecha_fin else 0
+                                    #porcentaje_avance = max(0, min(100, porcentaje_avance))
+                                #else:
+                                    #porcentaje_avance = 100 if fecha_actual >= fecha_fin else 0
                                     
                                 avance_data.append({
                                     'PEDIDO': pedido,
