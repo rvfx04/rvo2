d48aac0096076afacd80322e300cf93885821606
rvfx04 <rvfx04@gmail.com>
2025-02-27 16:44:10 -0500

    Update probadordecodigo.py

--- probadordecodigo.py
+++ probadordecodigo.py
@@ -463,9 +463,10 @@ if st.button("Ejecutar Consulta"):
                                 avance_data.append({
                                     'PEDIDO': pedido_original,  # Usar el formato original para mostrar
                                     'PROCESO': proceso,
-                                    'Avance_Real': avance_valor  # Nueva columna
+                                    'Avance_Real': avance_valor,  # Nueva columna
+                                    
                                     'Días(-)': diferencia_dias,
-                                    'Avance_Prog': f"{porcentaje_avance:.0f}%",
+                                    'Avance_Prog': f"{porcentaje_avance:.0f}%"
                                     
                                 })
                             except Exception as e:
